1c895effda732509b495b128df08bab4060fc489
alexcheuk <cheuk.alex@gmail.com>
2016-08-26 12:02:38 -0700

    pix server

--- server/socket.js
+++ server/socket.js
@@ -13,33 +13,38 @@ module.exports = function (io) {
         socket.on('handshake', function(user){
             var role = user.role || 'Uploader';
 
-            console.log('Attempt Handshake: ', user.email);
-
             console.log(role + ' Handhsaking: ' + user.email);
+            console.log('Handshake Socket: ', socket.id);
 
             user.roomKey = getUserKey(user);
 
             socket.user = user;
 
-            rooms[user.roomKey] = rooms[user.roomKey] || {
-                receiver : null,
+            rooms[socket.user.roomKey] = rooms[socket.user.roomKey] || {
+                receiver : [],
                 uploaders : []
             };
 
-            socket.room = user.room = rooms[user.roomKey];
-            
-            socket.join(user.roomKey, function(){
-                console.log(role + ' ' + user.email + ' joined room: ' + user.roomKey);
+            console.log(role + ' ' + socket.user.email + ' joining room ' + socket.user.roomKey);
 
-                if(user.role === 'receiver'){
-                    user.room.receiver = socket;
+            socket.user.room = rooms[socket.user.roomKey];
+
+            socket.join(socket.user.roomKey, function(){
+                console.log(role + ' ' + socket.user.email + ' joined room: ' + socket.user.roomKey);
+
+                if(socket.user.role === 'receiver'){
+                    socket.user.room.receiver.push(socket);
+
+                    console.log('receiver socket join: ', socket.id, socket.user.room.receiver.length);
                 }else{
-                    user.room.uploaders.push(socket);
+                    socket.user.room.uploaders.push(socket);
+
+                    console.log('uploader socket join: ', socket.id, socket.user.room.uploaders.length);
                 }
 
-                if(user.room.receiver && user.room.uploaders.length){
-                    console.log('Ready!');
-                	io.to(user.roomKey).emit('paired');
+                if(socket.user.room.receiver && socket.user.room.receiver.length && socket.user.room.uploaders.length){
+                    console.log('Room Ready!: ' + socket.user.roomKey);
+                	io.to(socket.user.roomKey).emit('paired');
                 }
             });
         });
@@ -49,28 +54,43 @@ module.exports = function (io) {
 
             console.log('Image Uploaded from Client ['+ user.email +']: ', imageData);
 
-            socket.room.receiver.emit('image-uploaded', imageData);
+            for (var i = 0; i < socket.user.room.receiver.length; i++) {
+                var receiver = socket.user.room.receiver[i];
+
+                receiver.emit('image-uploaded', imageData);
+            };
         });
 
         socket.on('disconnect', function(){
             if(!socket.user) return;
 
             if(socket.user.role === 'receiver'){
-                console.log('Receiver Left');
-                socket.room.receiver = null;
-                io.to(socket.user.roomKey).emit('not-ready');
-                return;
+
+                for (var i = 0; i < socket.user.room.receiver.length; i++) {
+                    var receiver = socket.user.room.receiver[i];
+
+                    if(receiver.id === socket.id){
+                        console.log('Receiver Left');
+                        socket.user.room.receiver.splice(i,1);
+                        break;
+                    }
+                };
+            }else{
+                for (var i = 0; i < socket.user.room.uploaders.length; i++) {
+                    var uploader = socket.user.room.uploaders[i];
+
+                    if(uploader.id === socket.id){
+                        console.log('Uploader Left');
+                        socket.user.room.uploaders.splice(i,1);
+                        break;
+                    }
+                };
             }
 
-            for (var i = 0; i < socket.room.uploaders.length; i++) {
-                var uploader = socket.room.uploaders[i];
-
-                if(uploader.id === socket.id){
-                    console.log('Uploader Left');
-                    socket.room.uploaders.splice(i,1);
-                    break;
-                }
-            };
+            if(socket.user.room.receiver.length === 0 || socket.user.room.uploaders.length === 0){
+                io.to(socket.user.roomKey).emit('not-ready');
+                console.log('Not Ready: '+ socket.user.roomKey);
+            }
         });
     });
 
